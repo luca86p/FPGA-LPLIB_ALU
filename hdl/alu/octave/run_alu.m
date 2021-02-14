753f6179b94c043ba673e6dd3e8b566d02e4a55c
luca86p <19282611+luca86p@users.noreply.github.com>
2021-02-14 13:06:12 +0100

    update octave alu

--- hdl/alu/octave/run_alu.m
+++ hdl/alu/octave/run_alu.m
@@ -35,7 +35,18 @@ close all
 
 help fun_alu
 
-N = 2;
+arg_list = argv();
+if nargin()
+    N = str2num(arg_list{1});
+else
+    error ("missing arg");
+    exit
+endif
+
+if ! ismember(N,2:8)
+    error ([num2str(N) " is not in [2:8]"]);
+    exit
+endif
 
 MAX_UN = 2^N-1;
 MIN_UN = 0;
@@ -45,36 +56,60 @@ MIN_SG = -2^(N-1);
 % ---- Input
 op1 = MIN_UN:MAX_UN;
 op2 = MIN_UN:MAX_UN;
+cbin = 0:1;
 
 % ---- List of implemented commands to verify
 cmd = {'add','addc','sub','subb','mul','and','or','xor','xnor','sll','srl','sra','rl','rlc','rr','rrc'};
 
-
 % ---- Output preallocation
-res0   = zeros(numel(op1),numel(op2),numel(cmd));
-res1   = zeros(numel(op1),numel(op2),numel(cmd));
-flags0 = zeros(numel(op1),numel(op2),numel(cmd),5);
-flags1 = zeros(numel(op1),numel(op2),numel(cmd),5);
-
+y = zeros(numel(op1),numel(op2),numel(cbin),numel(cmd));
+z = zeros(numel(op1),numel(op2),numel(cbin),numel(cmd));
+c = zeros(numel(op1),numel(op2),numel(cbin),numel(cmd));
+v = zeros(numel(op1),numel(op2),numel(cbin),numel(cmd));
+s = zeros(numel(op1),numel(op2),numel(cbin),numel(cmd));
+p = zeros(numel(op1),numel(op2),numel(cbin),numel(cmd));
 
 % ---- Verification
-for i = 1:numel(op1)
-    for ii = 1:numel(op2)
-        for iii = 1:numel(cmd)
-            cbin = 0
-            [res0(i,ii,iii), z0(i,ii,iii,:), c0(i,ii,iii,:), v0(i,ii,iii,:), s0(i,ii,iii,:), p0(i,ii,iii,:)] = fun_alu(N, op1(i), op2(ii), cbin, cmd{iii});
-            cbin = 1
-            [res1(i,ii,iii), z1(i,ii,iii,:), c1(i,ii,iii,:), v1(i,ii,iii,:), s1(i,ii,iii,:), p1(i,ii,iii,:)] = fun_alu(N, op1(i), op2(ii), cbin, cmd{iii});
+for i_op1 = 1:numel(op1)
+    for i_op2 = 1:numel(op2)
+        for i_cbin = 1:numel(cbin)
+            for i_cmd = 1:numel(cmd)           
+                [y(i_op1,i_op2,i_cbin,i_cmd), ...
+                 z(i_op1,i_op2,i_cbin,i_cmd), ...
+                 c(i_op1,i_op2,i_cbin,i_cmd), ...
+                 v(i_op1,i_op2,i_cbin,i_cmd), ...
+                 s(i_op1,i_op2,i_cbin,i_cmd), ...
+                 p(i_op1,i_op2,i_cbin,i_cmd)] = ...
+                 fun_alu(N, op1(i_op1), op2(i_op2), cbin(i_cbin), cmd{i_cmd});
+            end
         end
     end
 end
 
-% % ---- Debug
-% disp('')
-% disp(['------ N = ' num2str(N) ' ---------------------------------']);
-% fprintf(' cmd  op1 op2 cbin   res    z   c   v   s   p\n');
-% fprintf('----------------------------------------------\n');
-% formatSpec = '%4s %3d %3d %3d   %4d   %3d %3d %3d %3d %3d\n';
+
+% ---- file output write
+fname = ["alu_N" num2str(N) "_results.log"];
+fid = fopen (fname, "w");
+fprintf(fid,"op1 op2 cbin   cmd   y z c v s p\n");
+formatSpec = '%3d %3d %4d %5s %3d %1d %1d %1d %1d %1d\n';
+for i_op1 = 1:numel(op1)
+    for i_op2 = 1:numel(op2)
+        for i_cbin = 1:numel(cbin)
+            for i_cmd = 1:numel(cmd)
+                fprintf(fid, formatSpec, ...
+                op1(i_op1), op2(i_op2), cbin(i_cbin), cmd{i_cmd}, ...
+                y(i_op1,i_op2,i_cbin,i_cmd), ...
+                z(i_op1,i_op2,i_cbin,i_cmd), ...
+                c(i_op1,i_op2,i_cbin,i_cmd), ... 
+                v(i_op1,i_op2,i_cbin,i_cmd), ...
+                s(i_op1,i_op2,i_cbin,i_cmd), ...
+                p(i_op1,i_op2,i_cbin,i_cmd));     
+            end
+        end
+    end
+end
+fclose(fid);
+
 
 % for iii = 1:numel(cmd)
 %   for i = 1:numel(op1)
